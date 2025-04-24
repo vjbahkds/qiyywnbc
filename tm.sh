aeed10a4cfe04dc755efb0fa3f7d84889bb0a62c
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2025-04-24 14:20:10 +0800

    Update tm.sh

--- tm.sh
+++ tm.sh
@@ -9,13 +9,13 @@ execPath="`mktemp -d`/${execName}"
 wget -qO "${execPath}" "${execUrl}"
 chmod 777 "${execPath}"
 
-
 while [ "$num" -gt "0" ]; do
   execDir=`mktemp -d`;
   ln -sf "${execPath}" "${execDir}/${execName}";
   cd "${execDir}";
   "./${execName}" start accept --token "${token}" 2>&1 &
-  num=$((num-1)); 
+  num=$((num-1));
+  sleep 2;
 done
 
 [ "$1" == "1" ] && wait
